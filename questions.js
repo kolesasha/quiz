d7e087d844a8572c5ab17b5d1953046f5bd30ab3
kolesasha <62481829+kolesasha@users.noreply.github.com>
2021-04-25 21:12:41 +0200

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,72 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "How many times was Ross legally divorced?",
+    answer: "Three times",
+    options: [
+      "Twice",
+      "Three times",
+      "Five times",
+      "Six times"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Where did Carol first meet Susan?",
+    answer: "At the gym",
+    options: [
+      "In college",
+      "At work",
+      "At the gym",
+      "At Central Perk"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What were Ben's first words?",
+    answer: "Hi",
+    options: [
+      "Hi",
+      "Bye",
+      "Mom",
+      "Dumb"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What was Gunther's job before he worked at Central Perk?",
+    answer: "A soap opera actor",
+    options: [
+      "A history professor",
+      "A soap opera actor",
+      "A marketing executive",
+      "A fashion designer"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What was the name of the first restaurant Monica was head chef at?",
+    answer: "Alessandro's",
+    options: [
+      "Alessandro's",
+      "Moondance Diner",
+      "Iridium",
+      "Javu"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
